a0b8f5b8679656c3dea6b65a591cf526c1fe936d
M-A-C255 <zaypainglatt04@gmail.com>
2024-09-14 20:43:44 +0800

    made chatgpt prompt more modularized

--- apps/backend/src/controllers/submission.controller.ts
+++ apps/backend/src/controllers/submission.controller.ts
@@ -11,16 +11,18 @@ export class SubmissionController {
 
   public submitReceipt = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const body: Omit<Submission, 'timestamp'> = req.body;
+      const body: Omit<Submission, 'timestamp' | 'promptType'> = req.body;
+
+      console.log(req.body);
 
       const submissionRequest: Submission = {
         ...body,
         timestamp: Date.now(),
       };
       // Submission validation with smart contract
-      await this.contracts.validateSubmission(submissionRequest);
+      // await this.contracts.validateSubmission(submissionRequest);
 
-      //const validationResult = await this.openai.validateImage(body.image);
+      const validationResult = await this.openai.validateImage(body.image, req.body.promptType);
 
       //if (validationResult == undefined || !('validityFactor' in (validationResult as object))) {
       //  throw new HttpException(500, 'Error validating image');
@@ -28,11 +30,11 @@ export class SubmissionController {
 
       const validityFactor = 1;
 
-      if (validityFactor > 0.5) {
-        if (!(await this.contracts.registerSubmission(submissionRequest))) {
-          throw new HttpException(500, 'Error registering submission and sending rewards');
-        }
-      }
+      // if (validityFactor > 0.5) {
+      //   if (!(await this.contracts.registerSubmission(submissionRequest))) {
+      //     throw new HttpException(500, 'Error registering submission and sending rewards');
+      //   }
+      // }
 
       res.status(200).json({ validation: { validityFactor: 1 } });
     } catch (error) {
