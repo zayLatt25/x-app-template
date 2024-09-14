0d1243fe5260198688af5183621210347815aa62
Mac <zaypainglatt04@gmail.com>
2024-09-15 02:31:54 +0800

    Fix/chatgpt prompts (#15)
    * readded the missing files
    
    * added the missing categories
    
    * removed extra line
    
    * added prompt for tree planting
    
    * removed support for waste removal and volunteering

--- apps/backend/src/controllers/submission.controller.ts
+++ apps/backend/src/controllers/submission.controller.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 import { Container } from 'typedi';
 import { OpenaiService } from '@/services/openai.service';
 import { Submission } from '@/interfaces/submission.interface';
-import { HttpException } from '@/exceptions/HttpException';
 import { ContractsService } from '@/services/contracts.service';
 
 export class SubmissionController {
@@ -21,12 +20,7 @@ export class SubmissionController {
       // TODO: Submission validation with smart contract
       const validationResult = await this.openai.validateImage(body.image, req.body.promptType);
 
-      const validityFactor = validationResult['validityFactor'];
-      if (validityFactor < 0.5) {
-        throw new HttpException(500, 'Error registering submission and sending rewards');
-      }
-
-      res.status(200).json({ validation: { validityFactor: 1 } });
+      res.status(200).json({ validation: validationResult });
     } catch (error) {
       next(error);
       return;
