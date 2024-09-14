6c2c1d6e816ef40e46228db05607bbefcee9d157
WillowMT <waiyanmt.20@icloud.com>
2024-09-15 00:01:58 +0800

    login form ui fix

--- apps/frontend/src/routes/login.tsx
+++ apps/frontend/src/routes/login.tsx
@@ -9,20 +9,18 @@ export default function Login() {
     if (account) {
         navigate('/')
     }
-    return <div className="absolute inset-0 h-full grid place-items-center text-xl font-bold md:font-extrabold">
+    return <div className="absolute inset-0 h-full grid place-items-center">
 
-        <div className=" " data-x="bg-emerald-500 px-5 md:px-24 shadow-lg rounded-2xl">
-            <div className="max-w-xl bg-white p-8 rounded-2xl px-10">
-                <h2 className="text-3xl font-bold text-green-800 mb-2">SG Odyssey</h2>
+        <div className=" " data-x="bg-emerald-500 px-5 md:px-24 shadow-xl rounded-2xl">
+            <div className="max-w-md bg-white p-8 rounded-2xl px-10">
+                <h2 className="text-3xl font-bold text-green-800 mb-4">SG Odyssey</h2>
 
                 <p className="text-green-700 mb-6 text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
                 </p>
-                <form className="space-y-4">
-                    <input type="text" placeholder="Username" className="w-full px-4 py-3 rounded-full border-green-300 border" />
-                    <input type="password" placeholder="Password" className="w-full px-4 py-3 rounded-full border-green-300 border" />
+                <form className="">
+                    <input type="text" placeholder="Username" className="w-full px-2 py-1  rounded-full border-[#506c4c] border mb-2" />
+                    <input type="password" placeholder="Password" className="w-full px-2 py-1  rounded-full border-[#506c4c] border mb-6" />
 
                     <button className="w-full bg-[#506c4c] text-white rounded-full py-3 hover:bg-green-700 transition-colors">
 
