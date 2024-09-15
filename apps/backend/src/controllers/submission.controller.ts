a23233a64adb2ecc8fe0842be4c31f9131bdd9bc
Mac <zaypainglatt04@gmail.com>
2024-09-15 11:56:12 +0800

    final (#24)

--- apps/backend/src/controllers/submission.controller.ts
+++ apps/backend/src/controllers/submission.controller.ts
@@ -3,6 +3,7 @@ import { Container } from 'typedi';
 import { OpenaiService } from '@/services/openai.service';
 import { Submission } from '@/interfaces/submission.interface';
 import { ContractsService } from '@/services/contracts.service';
+import { HttpException } from '@/exceptions/HttpException';
 
 export class SubmissionController {
   public openai = Container.get(OpenaiService);
@@ -17,8 +18,21 @@ export class SubmissionController {
         timestamp: Date.now(),
       };
 
-      // TODO: Submission validation with smart contract
+      await this.contracts.validateSubmission(submissionRequest);
+
       const validationResult = await this.openai.validateImage(body.image, req.body.promptType);
+
+      if (validationResult == undefined || !('validityFactor' in (validationResult as object))) {
+        throw new HttpException(500, 'Error validating image');
+      }
+
+      const validityFactor = validationResult['validityFactor'];
+
+      if (validityFactor > 0.5) {
+        if (!(await this.contracts.registerSubmission(submissionRequest))) {
+          throw new HttpException(500, 'Error registering submission and sending rewards');
+        }
+      }
 
       res.status(200).json({ validation: validationResult });
     } catch (error) {
